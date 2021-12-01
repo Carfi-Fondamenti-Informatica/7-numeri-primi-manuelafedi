689444ae2c328d52e64943417d5dbca9eea161bd
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-12-01 18:30:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int n=0;
+  cin >>n;
+  if (ric(2,n)){
+    cout <<"numero primo" <<endl;
+  }else {
+    cout <<"numero non primo" <<endl;
+  }
   return 0;
 }
