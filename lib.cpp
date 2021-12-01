34e5dd1e5835cbd2a849b818567fec3a2f235720
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-12-01 18:42:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "lib.h"
+bool ric (int i, int n){
+  if (i==n){
+    return true;
+  }else if (n==1){
+    return true;
+  }else if (n%i==0){
+    return false;
+  }else{
+    return ric(i+1,n);
+  }
+  
+}
